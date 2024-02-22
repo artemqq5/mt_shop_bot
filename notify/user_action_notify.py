00590badd8446c1d050bc7c83fac5c04368a9c71
artem <artemqq5@gmail.com>
2024-02-22 13:14:44 +0200

    add fast fix

--- notify/user_action_notify.py
+++ notify/user_action_notify.py
@@ -7,7 +7,7 @@ async def user_activate_bot(telegram_id, bot):
         admins = UsersRepository().get_users(position=ADMIN)
         user = UsersRepository().get_user(telegram_id)
 
-        message = f"👤 <b>Новий користувач доєднався до боту!</b>\n\nІм'я: <b>{user['name']}</b>\nТелеграм ID: <b>{user['id']}</b>\nЧас: <b>{user['time']}</b>"
+        message = f"👤 <b>Новий користувач доєднався до боту!</b>\n\nІм'я: <b>@{user['name']}</b>\nТелеграм ID: <b>{user['id']}</b>\nЧас: <b>{user['time']}</b>"
 
         for admin in admins:
             await bot.send_message(chat_id=admin['id'], text=message)

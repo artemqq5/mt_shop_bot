173b2fbdb4bf904311196b868f627b39dca3e6dd
artem <artemqq5@gmail.com>
2025-07-25 18:29:33 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "🔁 Running Alembic migrations..."
-alembic upgrade head
+#echo "🔁 Running Alembic migrations..."
+#alembic upgrade head
 
 echo "🚀 Starting bot..."
-exec python -m uvicorn bot.main:app --host 0.0.0.0 --port 8006
+exec python -m uvicorn bot.main:app --host 0.0.0.0 --port 8007
